1d317ddcd2f99e3b000518f7079616446de843a9
eric <eric97@vt.edu>
2019-08-16 13:36:27 -0400

    housed queries in function

--- SC/demo_data.py
+++ SC/demo_data.py
@@ -6,33 +6,37 @@ conn = sqlite3.connect('demo_data.sqlite3')
 curs = conn.cursor()
 
 
+def query(string):
+    curs.execute(string)
+
+def save():
+    curs.close()
+    conn.commit()
+
+
 # Create a table
-query = '''CREATE TABLE demo (s TEXT,
+query('''CREATE TABLE demo (s TEXT,
                               x NUMERIC,
-                              Y numeric);'''
-curs.execute(query)
+                              Y numeric);''')
+
 
 
 # Insert Rows
-query = '''INSERT INTO demo VALUES ('g', 3, 9);'''
-curs.execute(query)
+query('''INSERT INTO demo VALUES ('g', 3, 9);''')
 
-query = '''INSERT INTO demo VALUES ('v', 5, 7);'''
-curs.execute(query)
 
-query = '''INSERT INTO demo VALUES ('f', 8, 7);'''
-curs.execute(query)
+query('''INSERT INTO demo VALUES ('v', 5, 7);''')
+
+query('''INSERT INTO demo VALUES ('f', 8, 7);''')
 
 
 # Check
-query = '''SELECT * FROM demo;'''
-curs.execute(query)
+query('''SELECT * FROM demo;''')
 print('Data check:', curs.fetchall())
 
 
 # Close connection
-curs.close()
-conn.commit()
+save()
 
 ####################################################
 
@@ -42,26 +46,23 @@ curs = conn.cursor()
 
 
 # How many rows?
-query = '''SELECT COUNT(*) FROM demo;'''
-curs.execute(query)
+query('''SELECT COUNT(*) FROM demo;''')
 
 # Print answer
 print('There are', curs.fetchone()[0], 'rows.')
 
 
 # How many rows where x & y are at least 5?
-query = '''SELECT COUNT(*) FROM demo
+query('''SELECT COUNT(*) FROM demo
 WHERE x >= 5
-AND y >= 5;'''
-curs.execute(query)
+AND y >= 5;''')
 
 # Print answer
 print('There are', curs.fetchone()[0], 'rows where x and y are at least 5.')
 
 
 # How many unique values of y are there
-query = '''SELECT COUNT(DISTINCT y) FROM demo;'''
-curs.execute(query)
+query('''SELECT COUNT(DISTINCT y) FROM demo;''')
 
 # Print answer
 print('There are', curs.fetchone()[0], 'unique values of y.')
